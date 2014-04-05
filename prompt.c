428c3ae6c2d0de71fdb0b7336465f70fce019c3e
Ryan Linton <ryanlntn@gmail.com>
2014-04-05 16:43:06 -0700

    Add extra message to version and info

--- prompt.c
+++ prompt.c
@@ -31,6 +31,7 @@ void add_history(char* unused) {}
 int main(int argc, char** argv) {
 
   puts("Something Version 0.0.0.0.1");
+  puts("Lovingly crafted by ryanlntn.");
   puts("Press Ctrl+c to Exit\n");
 
   while (1) {

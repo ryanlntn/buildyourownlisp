5f3b420d886c69ef64ddfd0825e264090e08410e
Ryan Linton <ryanlntn@gmail.com>
2014-04-05 16:26:23 -0700

    Rename prompt

--- prompt.c
+++ prompt.c
@@ -10,7 +10,7 @@ static char buffer[2048];
 
 /* Fake readline function */
 char* readline(char* prompt) {
-  fputs("lispy> ", stdout);
+  fputs("something> ", stdout);
   fgets(buffer, 2048, stdin);
   char* cpy = malloc(strlen(buffer)+1);
   strcpy(cpy, buffer);
@@ -30,13 +30,13 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv) {
 
-  puts("Lispy Version 0.0.0.0.1");
+  puts("Something Version 0.0.0.0.1");
   puts("Press Ctrl+c to Exit\n");
 
   while (1) {
 
     /* Now in either case readline will be correctly defined */
-    char* input = readline("lispy> ");
+    char* input = readline("something> ");
     add_history(input);
 
     printf("No you're a %s\n", input);

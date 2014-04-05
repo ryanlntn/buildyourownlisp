4024325a6f98a09d982e622ae8c1431b01affbfc
Ryan Linton <ryanlntn@gmail.com>
2014-04-05 10:27:12 -0700

    Get editline working

--- lispy.c
+++ lispy.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a static buffer for user input of maximum size 2048 */
-static char input[2048];
+#include <editline/readline.h>
 
 int main(int argc, char** argv) {
 
@@ -11,15 +11,19 @@ int main(int argc, char** argv) {
 
   /* In a never ending loop */
   while (1) {
-  
-    /* Output our prompt */
-    fputs("lispy> ", stdout);
-  
-    /* Read a line of user input of maximum size 2048 */
-    fgets(input, 2048, stdin);
-  
+    
+    /* Output our prompt and get input */
+    char* input = readline("lispy> ");
+
+    /* Add input to history */
+    add_history(input);
+
     /* Echo input back to user */
-    printf("No you're a %s", input);
+    printf("No you're a %s\n", input);
+
+    /* Free retrived input */
+    free(input);
+
   }
 
   return 0;
